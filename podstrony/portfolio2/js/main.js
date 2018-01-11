a065aa2e61c2cbee610d4430227d4e0da18d85b6
Kajetan Bystry <kajetanbystry@gmail.com>
2018-01-11 12:07:02 +0100

    more

--- podstrony/portfolio2/js/main.js
+++ podstrony/portfolio2/js/main.js
@@ -1,15 +1,4 @@
 $(document).ready (function(){
-   $('#navbar').find('a').click(function(){
-       if(this.hash !== ''){
-           event.preventDefault();
-           var hash = this.hash;
-           $('html, body').animate({
-               scrollTop: $(hash).offset().top
-           }, 800, function(){
-               window.location.hash = hash;
-           })
-       }
-   })
     changeNavbarColor();
 });
 
@@ -34,5 +23,32 @@ function changeNavbarColor(){
     
 }
 
+$(document).ready (function(){
+   $('#navbar').find('a').click(function(){
+       if(this.hash !== ''){
+           event.preventDefault();
+           var hash = this.hash;
+           $('html, body').animate({
+               scrollTop: $(hash).offset().top
+           }, 800, function(){
+               window.location.hash = hash;
+           })
+       }
+   }) 
+});
+
+$(document).ready (function(){
+   $('#footerlinks').find('a').click(function(){
+       if(this.hash !== ''){
+           event.preventDefault();
+           var hash = this.hash;
+           $('html, body').animate({
+               scrollTop: $(hash).offset().top
+           }, 800, function(){
+               window.location.hash = hash;
+           })
+       }
+   }) 
+});
 
 
