6b8285d5edac891894e14a509de9d4516941173f
Kajetan Bystry <kajetanbystry@gmail.com>
2018-02-21 12:41:58 +0100

    ang

--- js/main.js
+++ js/main.js
@@ -29,6 +29,17 @@ $('#navbtn').click(function(){
     $('.nav-item').css('background-color', '#FFF');
 })
 
+$('#langbtn').click(function(){
+    if($('#langbtn').hasClass('expanded')){
+        $('#eng').fadeOut();
+        $('#langbtn').removeClass('expanded');
+        $('#arrow').removeClass('fa-caret-up').addClass('fa-caret-down');
+    }else{
+        $('#eng').fadeIn();
+    $('#arrow').removeClass('fa-caret-down').addClass('fa-caret-up');
+    $('#langbtn').addClass('expanded');
+        }
+})
 //$('#enter').click(function(){
 //    $('#welcome').css('margin-top', '60px');
 //    $('#navbar').css('z-index', '1');
